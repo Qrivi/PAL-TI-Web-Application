aeab0e9905ff3d3b8032880e8c21f6badf30ba32
Matthias <matthias_hannes@hotmail.com>
2016-02-12 12:11:54 +0100

    TutorController package

--- src/main/java/be/peerassistedlearningti/web/controller/TutorController.java
+++ src/main/java/be/peerassistedlearningti/web/controller/TutorController.java
@@ -1,3 +1,8 @@
-public class TutorController {
+package be.peerassistedlearningti.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class TutorController {
+    @Autowired
+    private PALService service;
 }
