21edd4b9a9e3489cc2a87c2b71d6cfcd2c803153
David Op de Beeck <davidopdebeeck@hotmail.com>
2016-02-13 01:08:44 +0100

    Campus and RoomType converter use the service class

--- src/main/java/be/peerassistedlearningti/web/model/converter/CampusConverter.java
+++ src/main/java/be/peerassistedlearningti/web/model/converter/CampusConverter.java
@@ -1,13 +1,20 @@
 package be.peerassistedlearningti.web.model.converter;
 
 import be.peerassistedlearningti.model.Campus;
+import be.peerassistedlearningti.service.PALService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.converter.Converter;
 
 /**
  * Converts a String into the matching Campus
  */
-public class CampusConverter implements Converter<String, Campus> {
-    public Campus convert(String s) {
-        return Campus.getByValue(s);
+public class CampusConverter implements Converter<String, Campus>
+{
+    @Autowired
+    private PALService service;
+
+    public Campus convert( String s )
+    {
+        return service.getCampusByName( s );
     }
 }
--- src/main/java/be/peerassistedlearningti/web/model/converter/RoomTypeConverter.java
+++ src/main/java/be/peerassistedlearningti/web/model/converter/RoomTypeConverter.java
@@ -2,13 +2,20 @@ package be.peerassistedlearningti.web.model.converter;
 
 
 import be.peerassistedlearningti.model.RoomType;
+import be.peerassistedlearningti.service.PALService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.converter.Converter;
 
 /**
  * Converts a String into the matching RoomType
  */
-public class RoomTypeConverter implements Converter<String, RoomType> {
-    public RoomType convert(String s) {
-        return RoomType.getByValue(s);
+public class RoomTypeConverter implements Converter<String, RoomType>
+{
+    @Autowired
+    private PALService service;
+
+    public RoomType convert( String s )
+    {
+        return service.getRoomTypeByType( s );
     }
 }
