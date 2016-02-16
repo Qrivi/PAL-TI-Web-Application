51f7a90cb3b17b00f3cdef9b75ab541c71dc8e2a
Koen_DE <koen.demonie1992@gmail.com>
2016-02-16 19:50:32 +0100

    Fixed imports

--- src/main/java/be/peerassistedlearningti/web/model/validation/CheckWithSessionPassword.java
+++ src/main/java/be/peerassistedlearningti/web/model/validation/CheckWithSessionPassword.java
@@ -1,15 +1,13 @@
 package be.peerassistedlearningti.web.model.validation;
 
-import be.peerassistedlearningti.web.model.validation.CheckWithSessionPasswordValidator;
-
-import static java.lang.annotation.ElementType.*;
-import static java.lang.annotation.RetentionPolicy.*;
-
 import javax.validation.Constraint;
 import javax.validation.Payload;
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 @Target( { METHOD , FIELD , ANNOTATION_TYPE } )
 @Retention( RUNTIME )
