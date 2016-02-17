584802845e92163d6922107148654a463a0e189c
Koen_DE <koen.demonie1992@gmail.com>
2016-02-17 15:13:08 +0100

    Removed StudentService

--- src/main/java/be/peerassistedlearningti/web/controller/DefaultController.java
+++ src/main/java/be/peerassistedlearningti/web/controller/DefaultController.java
@@ -1,27 +1,15 @@
 package be.peerassistedlearningti.web.controller;
 
-import be.peerassistedlearningti.dao.CourseDAO;
-import be.peerassistedlearningti.dao.StudentDAO;
-import be.peerassistedlearningti.model.Course;
-import be.peerassistedlearningti.model.Student;
-import be.peerassistedlearningti.repository.StudentRepository;
-import be.peerassistedlearningti.repository.StudentService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.Collection;
 
 
 @Controller
 @RequestMapping( "/" )
 public class DefaultController
 {
-
-    @Autowired
-    StudentService studentService;
 
     @ResponseBody
     @RequestMapping( method = RequestMethod.GET )
