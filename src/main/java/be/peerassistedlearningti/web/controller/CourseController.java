4c7c0594d7ac7ebadc92bb4b757e0e90925b9892
Matthias <matthias_hannes@hotmail.com>
2016-02-17 21:32:15 +0100

    Updated CourseController for courseOverview

--- src/main/java/be/peerassistedlearningti/web/controller/CourseController.java
+++ src/main/java/be/peerassistedlearningti/web/controller/CourseController.java
@@ -8,12 +8,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping(value = "/course")
 public class CourseController {
     @Autowired
     private PALService service;
+
+    @RequestMapping( value = "/course/overview", method = RequestMethod.GET )
+    public ModelAndView getCourseOverviewPage()
+    {
+        return new ModelAndView("course", "courses", service.getAllCourses());
+    }
 
     @RequestMapping(value = "/subscribe", method = RequestMethod.POST)
     public String subscribeToCourse( @PathVariable( value = "id" ) int id )
