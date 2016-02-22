ee44a870fb211efac7b7d340a1bb346bc0cc1d05
Koen_DE <koen.demonie1992@gmail.com>
2016-02-22 22:34:52 +0100

    Created class to list all timeline events

---
+++ src/main/java/be/peerassistedlearningti/web/model/util/Timeline.java
@@ -0,0 +1,38 @@
+package be.peerassistedlearningti.web.model.util;
+
+import be.peerassistedlearningti.model.Student;
+import be.peerassistedlearningti.service.PALService;
+import be.peerassistedlearningti.util.TimelineObject;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Comparator;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+/**
+ * Class used to create a timeline of different events
+ */
+public class Timeline {
+
+    @Autowired
+    private PALService service;
+    private SortedSet<TimelineObject> objects = new TreeSet<TimelineObject>(new TimelineObjectCmp());
+
+    public Timeline(Student student) {
+        objects.addAll(service.getPastLessons(student.getEmail()));
+        objects.addAll(service.getReviewsForStudent(student));
+    }
+
+    public Set<TimelineObject> getTimeline() {
+        return objects;
+    }
+
+    //Comparator
+    private class TimelineObjectCmp implements Comparator<TimelineObject> {
+
+        public int compare(TimelineObject o1, TimelineObject o2) {
+            return o1.getTimelineDate().compareTo(o2.getTimelineDate());
+        }
+    }
+}
