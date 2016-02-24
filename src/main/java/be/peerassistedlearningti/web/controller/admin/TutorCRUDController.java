63d439106e312c1e13a3c9681fbd9fc457c0cc13
Matthias <matthias_hannes@hotmail.com>
2016-02-24 20:05:56 +0100

    Added remove method in TutorCRUDController

--- src/main/java/be/peerassistedlearningti/web/controller/admin/TutorCRUDController.java
+++ src/main/java/be/peerassistedlearningti/web/controller/admin/TutorCRUDController.java
@@ -1,11 +1,13 @@
 package be.peerassistedlearningti.web.controller.admin;
 
+import be.peerassistedlearningti.model.Tutor;
 import be.peerassistedlearningti.service.PALService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -17,5 +19,13 @@ public class TutorCRUDController extends AdminController{
     public ModelAndView getTutorOverviewPage(ModelMap model )
     {
         return new ModelAndView("admin/tutors", "tutors", service.getAllTutors() );
+    }
+
+    @RequestMapping( value = "/tutors", method = RequestMethod.DELETE )
+    public String removeTutor( @RequestParam( required = true ) int id )
+    {
+        Tutor t = service.getTutorById( id );
+        service.removeTutor( t );
+        return "redirect:/admin/tutors";
     }
 }
