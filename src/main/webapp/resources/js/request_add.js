481333f1ca29611105c75a5160e398c06fbc53cc
Koen.Demonie <pW140026701!>
2016-02-28 18:51:54 +0100

    Fixed wrong id's

--- src/main/webapp/resources/js/request_add.js
+++ src/main/webapp/resources/js/request_add.js
@@ -3,12 +3,12 @@ $(document).ready(function() {
      * Count characters remaining on the review text field
      */
     var text_max = 300;
-    $('#review_text_feedback').html(text_max + ' characters remaining');
+    $('#request_text_feedback').html(text_max + ' characters remaining');
 
-    $('#review_text').keyup(function() {
-        var text_length = $('#review_text').val().length;
+    $('#request_text').keyup(function() {
+        var text_length = $('#request_text').val().length;
         var text_remaining = text_max - text_length;
 
-        $('#review_text_feedback').html(text_remaining + ' characters remaining');
+        $('#request_text_feedback').html(text_remaining + ' characters remaining');
     });
 }
