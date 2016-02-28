522b96a98edb2c28b444f1bf5133a7cb289835bb
Koen.Demonie <pW140026701!>
2016-02-28 18:45:32 +0100

    Added courses list model attribute

--- src/main/java/be/peerassistedlearningti/web/controller/student/RequestController.java
+++ src/main/java/be/peerassistedlearningti/web/controller/student/RequestController.java
@@ -5,6 +5,7 @@ import be.peerassistedlearningti.service.PALService;
 import be.peerassistedlearningti.web.model.form.RequestForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -18,7 +19,10 @@ public class RequestController extends StudentController {
     @RequestMapping(method = RequestMethod.GET)
     public ModelAndView getRequestPage()
     {
-        return new ModelAndView("student/request_add", "request", new RequestForm());
+        ModelMap model = new ModelMap();
+        model.addAttribute("request", new RequestForm());
+        model.addAttribute("courses", service.getAllCourses());
+        return new ModelAndView("student/request_add",model );
     }
 
 }
