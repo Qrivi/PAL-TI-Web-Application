9183bda1c20c0a4fca5c76c1d3fa39f4322f1b61
Koen.Demonie <pW140026701!>
2016-02-28 17:10:38 +0100

    Created RequestController Added getRequestPage.GET

---
+++ src/main/java/be/peerassistedlearningti/web/controller/student/RequestController.java
@@ -0,0 +1,24 @@
+package be.peerassistedlearningti.web.controller.student;
+
+
+import be.peerassistedlearningti.service.PALService;
+import be.peerassistedlearningti.web.model.form.RequestForm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping (value = "/request")
+public class RequestController extends StudentController {
+    @Autowired
+    private PALService service;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView getRequestPage()
+    {
+        return new ModelAndView("student/request_add", "request", new RequestForm());
+    }
+
+}
