8e611dec1129eeacc4568d068f1954fd1ea63632
Koen.Demonie <pW140026701!>
2016-02-29 17:44:31 +0100

    Added text_remaining coloring

--- src/main/webapp/resources/js/request_add.js
+++ src/main/webapp/resources/js/request_add.js
@@ -9,6 +9,20 @@ $(document).ready(function() {
         var text_length = $('#request_text').val().length;
         var text_remaining = text_max - text_length;
 
+        if(text_remaining>=20){
+            $("#request_text_feedback").removeClass("label-danger");
+            $("#request_text_feedback").removeClass("label-warning");
+            $("#request_text_feedback").addClass("label-default");
+        }else if(text_remaining<20 && text_remaining!=0){
+            $("#request_text_feedback").removeClass("label-default");
+            $("#request_text_feedback").removeClass("label-danger");
+            $("#request_text_feedback").addClass("label-warning");
+        }else {
+            $("#request_text_feedback").removeClass("label-default");
+            $("#request_text_feedback").removeClass("label-warning");
+            $("#request_text_feedback").addClass("label-danger");
+        }
+
         $('#request_text_feedback').html(text_remaining + ' characters remaining');
     });
-}
+});
