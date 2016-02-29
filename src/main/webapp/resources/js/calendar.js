588c91447f7892ddeb22e893d354dcb9b8f1067c
David Op de Beeck <davidopdebeeck@hotmail.com>
2016-02-29 23:35:10 +0100

    Calendar fills page

--- src/main/webapp/resources/js/calendar.js
+++ src/main/webapp/resources/js/calendar.js
@@ -5,9 +5,16 @@ $( document ).ready( function () {
             center : 'title' ,
             right  : 'month,agendaWeek,agendaDay'
         } ,
+        height     : window.innerHeight - 90 ,
         editable   : false ,
         eventLimit : true ,
         events     : "/calendar/events"
     } );
+
+    $( window ).resize( function () {
+        var calHeight = window.innerHeight - 90;
+        $( '#calendar' ).fullCalendar( 'option' , 'height' , calHeight );
+    } );
+
     setInterval( function () { $( '#calendar' ).fullCalendar( 'refresh' ); } , 60000 );
 } );
