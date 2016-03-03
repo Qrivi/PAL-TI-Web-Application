40c1926fadb19aad72143f3bb25361afc177f6a4
David Op de Beeck <davidopdebeeck@hotmail.com>
2016-03-03 04:31:04 +0100

    Added auto upvote when request is added

--- src/main/webapp/resources/js/request_add.js
+++ src/main/webapp/resources/js/request_add.js
@@ -1,83 +1,88 @@
-$(document).ready(function() {
+$( document ).ready( function () {
+    $( "#request_course" ).select2();
+    $( "#request-overview" ).DataTable( {
+        "order" : [
+            [
+                0 ,
+                "desc"
+            ]
+        ]
+    } );
     /**
      * Count characters remaining on the review text & title fields
      */
     var text_max = 300;
     var title_max = 50;
-    $('#request_text_feedback').html(text_max + ' characters remaining');
-    $('#request_title_feedback').html(title_max + ' characters remaining');
+    $( '#request_text_feedback' ).html( text_max + ' characters remaining' );
+    $( '#request_title_feedback' ).html( title_max + ' characters remaining' );
 
-    $('#request_text').keyup(function() {
-        var text_length = $('#request_text').val().length;
+    $( '#request_text' ).keyup( function () {
+        var text_length = $( '#request_text' ).val().length;
         var text_remaining = text_max - text_length;
 
-        if(text_remaining>=20){
-            $("#request_text_feedback").removeClass("label-danger");
-            $("#request_text_feedback").removeClass("label-warning");
-            $("#request_text_feedback").addClass("label-default");
-        }else if(text_remaining<20 && text_remaining!=0){
-            $("#request_text_feedback").removeClass("label-default");
-            $("#request_text_feedback").removeClass("label-danger");
-            $("#request_text_feedback").addClass("label-warning");
-        }else {
-            $("#request_text_feedback").removeClass("label-default");
-            $("#request_text_feedback").removeClass("label-warning");
-            $("#request_text_feedback").addClass("label-danger");
+        if ( text_remaining >= 20 ) {
+            $( "#request_text_feedback" ).removeClass( "label-danger" );
+            $( "#request_text_feedback" ).removeClass( "label-warning" );
+            $( "#request_text_feedback" ).addClass( "label-default" );
+        }
+        else if ( text_remaining < 20 && text_remaining != 0 ) {
+            $( "#request_text_feedback" ).removeClass( "label-default" );
+            $( "#request_text_feedback" ).removeClass( "label-danger" );
+            $( "#request_text_feedback" ).addClass( "label-warning" );
+        }
+        else {
+            $( "#request_text_feedback" ).removeClass( "label-default" );
+            $( "#request_text_feedback" ).removeClass( "label-warning" );
+            $( "#request_text_feedback" ).addClass( "label-danger" );
         }
 
-        $('#request_text_feedback').html(text_remaining + ' characters remaining');
-    });
-    $('#request_title').keyup(function() {
-        var text_length = $('#request_title').val().length;
+        $( '#request_text_feedback' ).html( text_remaining + ' characters remaining' );
+    } );
+    $( '#request_title' ).keyup( function () {
+        var text_length = $( '#request_title' ).val().length;
         var text_remaining = title_max - text_length;
-        if(text_remaining>=5){
-            $("#request_title_feedback").removeClass("label-danger");
-            $("#request_title_feedback").removeClass("label-warning");
-            $("#request_title_feedback").addClass("label-default");
-        }else if(text_remaining<5 && text_remaining!=0){
-            $("#request_title_feedback").removeClass("label-default");
-            $("#request_title_feedback").removeClass("label-danger");
-            $("#request_title_feedback").addClass("label-warning");
-        }else {
-            $("#request_title_feedback").removeClass("label-default");
-            $("#request_title_feedback").removeClass("label-warning");
-            $("#request_title_feedback").addClass("label-danger");
+        if ( text_remaining >= 5 ) {
+            $( "#request_title_feedback" ).removeClass( "label-danger" );
+            $( "#request_title_feedback" ).removeClass( "label-warning" );
+            $( "#request_title_feedback" ).addClass( "label-default" );
+        }
+        else if ( text_remaining < 5 && text_remaining != 0 ) {
+            $( "#request_title_feedback" ).removeClass( "label-default" );
+            $( "#request_title_feedback" ).removeClass( "label-danger" );
+            $( "#request_title_feedback" ).addClass( "label-warning" );
+        }
+        else {
+            $( "#request_title_feedback" ).removeClass( "label-default" );
+            $( "#request_title_feedback" ).removeClass( "label-warning" );
+            $( "#request_title_feedback" ).addClass( "label-danger" );
         }
 
-        $('#request_title_feedback').html(text_remaining + ' characters remaining');
-    });
-    $('#request_title').focusout(validRequestCheck);
-    $('#request_course').focusout(validRequestCheck);
-
-
-});
+        $( '#request_title_feedback' ).html( text_remaining + ' characters remaining' );
+    } );
+    $( '#request_title' ).change( validRequestCheck );
+    $( '#request_course' ).change( validRequestCheck );
+} );
 
 function validRequestCheck () {
-    var text_length = $('#request_title').val().length;
-    if (text_length >= 5 && $("#request_course").val() != "") {
+    var text_length = $( '#request_title' ).val().length;
+    if ( text_length >= 5 && $( "#request_course" ).val() != "" ) {
         updateSimilarRequests();
     }
 }
 
-function updateSimilarRequests() {
-    //Animate loading
-    var loading_html = "<div class=\"loading\">\n<i class=\"fa fa-refresh fa-spin\"></i>\n</div>";
-    $("#similar_requests").empty();
-    $(loading_html).hide().appendTo("#loading_similar").fadeIn(500);
-
+function updateSimilarRequests () {
+    $( "#similar_requests" ).empty();
     var request = {
-        title: $("#request_title").val(),
-        course: $("#request_course").val()
+        title  : $( "#request_title" ).val() ,
+        course : $( "#request_course" ).val()
     };
-    //Do ajax
-    $.ajax({
-        url: "/request/similar",
-        type: 'POST',
-        dataType: 'html',
-        data: request,
-        success: function (html) {
-            $("#loading_similar").find(".loading").fadeOut(1000);
-            $(html).hide().appendTo("#similar_requests").fadeIn(1000);
+    $.ajax( {
+        url      : "/request/similar" ,
+        type     : 'POST' ,
+        dataType : 'html' ,
+        data     : request ,
+        success  : function ( html ) {
+            $( html ).hide().appendTo( "#similar_requests" ).fadeIn( 1000 );
         }
-    });
+    } );
 }
