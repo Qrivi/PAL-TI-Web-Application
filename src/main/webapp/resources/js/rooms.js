dfd91160380c7b36bb7d56fccfb5e59823832fc6
Kristof Dewilde <me@krivi.be>
2016-09-05 02:59:02 +0200

    Fixed unresponsive buttons in paged tables

--- src/main/webapp/resources/js/rooms.js
+++ src/main/webapp/resources/js/rooms.js
@@ -1,12 +1,12 @@
-$( document ).ready( function () {
-    $( "#room-overview" ).DataTable();
-    $( ".campus" ).select2();
-    $( ".type" ).select2();
-    $( "#room-overview .update" ).click( function () {
-        var props = $( this ).parent().parent().find( "td" );
-        $( "#updateRoom #id" ).val( $( this ).attr( "data-id" ) );
-        $( "#updateRoom #name" ).val( props[ 0 ].textContent );
-        $( "#updateRoom #campus" ).val( props[ 1 ].textContent ).trigger( "change" );
-        $( "#updateRoom #type" ).val( props[ 2 ].textContent ).trigger( "change" );
-    } );
-} );
+$(document).ready(function () {
+    $("#room-overview").DataTable();
+    $(".campus").select2();
+    $(".type").select2();
+    $(document).on("click", "#room-overview .update", function () {
+        var props = $(this).parent().parent().find("td");
+        $("#updateRoom #id").val($(this).attr("data-id"));
+        $("#updateRoom #name").val(props[0].textContent);
+        $("#updateRoom #campus").val(props[1].textContent).trigger("change");
+        $("#updateRoom #type").val(props[2].textContent).trigger("change");
+    });
+});
